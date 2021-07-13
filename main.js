f82376b79d72650820e74e22c73b42c9101de1c5
himangshu06 <76275772+himangshu06@users.noreply.github.com>
2021-07-13 19:26:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+console.log("ml5Version:",ml5.version);
+Classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json",modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("ModelLoaded!");
+}
+
+function draw(){
+image(video,0,0,300,300);
+Classifier.classify(video,gotResult);
+}
+
+function gotResult(error,results){
+  if (error){
+      console.error(error);
+
+  }
+  else{
+console.log(results);
+document.getElementById("result_object").innerHTML=results[0].label;
+document.getElementById("result_object_acccuracy").innerHTML=results[0].confidence.toFixed(3);
+
+}
+}
+
